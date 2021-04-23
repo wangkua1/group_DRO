95db12efe8ce1ea9ad73a0eda44a16710a06f0a5
wangkua1 <wangkua1@cs.toronto.edu>
2021-04-22 21:36:06 -0400

    mkdir

--- run_scripts/setting_paths.sh
+++ run_scripts/setting_paths.sh
@@ -1,4 +1,5 @@
 CELEBA_ROOT=/scratch/ssd002/datasets/celeba/
+mkdir -p data_path/CelebA
 ln -s ${CELEBA_ROOT}/Eval/list_eval_partition.txt data_path/CelebA/list_eval_partition.csv
 ln -s ${CELEBA_ROOT}/Anno/list_attr_celeba.txt data_path/CelebA/list_attr_celeba.csv
 ln -s ${CELEBA_ROOT}/Img/img_align_celeba/ data_path/CelebA/img_align_celeba
